6197ab3dac83782e139efd5a06d5ebe2791fcb23
kentbrian <kentbrian0@gmail.com>
2022-01-13 23:03:17 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,9 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">SLACKA</a> and{' '}
+      Built with <a href="https://contentful.com/">SLACKA</a> {' '}
       <a href="https://gatsbyjs.com"></a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      <a href="https://github.com/contentful/starter-gatsby-blog">Source Code</a>
     </div>
   </Container>
 )
