a2efc4508ddbd8e77371b7a5c398e746d1a86eb8
kentbrian <kentbrian0@gmail.com>
2022-01-13 22:12:51 +0300

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,8 +5,7 @@ import * as styles from './navigation.module.css'
 
 const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
-    <Link to="/" className={styles.logoLink}>
-      <span className={styles.logo} />
+   
       <span className={styles.navigationItem}>My Portfolio</span>
     </Link>
     <ul className={styles.navigation}>
